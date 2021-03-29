9cd0738d0b5a4d24f1ff0d38bfedd998dce10170
Farouk <farouk.mokhtar@gmail.com>
2021-03-29 09:47:31 -0700

    add get_all_data.sh

---
+++ scripts/get_all_data.sh
@@ -0,0 +1,24 @@
+
+#!/bin/bash
+set -e
+
+rm -Rf test_tmp_delphes
+mkdir test_tmp_delphes
+cd test_tmp_delphes
+
+mkdir -p experiments
+mkdir -p data/pythia8_ttbar
+mkdir -p data/pythia8_ttbar/raw
+mkdir -p data/pythia8_ttbar/processed
+
+cd data/pythia8_ttbar/raw/
+
+for j in {0..10}
+do
+  for i {0..49}
+  do
+    wget --no-check-certificate -nc https://zenodo.org/record/4452283/files/tev14_pythia8_ttbar_"$j"_"$i".pkl.bz2
+  done
+done
+
+bzip2 -d *
