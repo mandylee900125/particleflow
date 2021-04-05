ab600fee6e6a5e9bd82e8f3cb9037d16228c679d
Farouk <farouk.mokhtar@gmail.com>
2021-04-05 13:28:35 -0700

    get qcd on prp

--- scripts/get_all_data.sh
+++ scripts/get_all_data.sh
@@ -1,36 +1,36 @@
 
-# #!/bin/bash
-# set -e
-#
-# rm -Rf test_tmp_delphes
-# mkdir test_tmp_delphes
-# cd test_tmp_delphes
-#
-# mkdir -p experiments
-#
-# mkdir -p data/pythia8_ttbar
-# mkdir -p data/pythia8_ttbar/raw
-# mkdir -p data/pythia8_ttbar/processed
-#
-# mkdir -p data/pythia8_qcd
-# mkdir -p data/pythia8_qcd/raw
-# mkdir -p data/pythia8_qcd/processed
+#!/bin/bash
+set -e
 
-# # now get the ttbar data for training/testing
-# cd data/pythia8_ttbar/raw/
-#
-# for j in {0..9}
-# do
-#   for i in {0..49}
-#   do
-#     wget --no-check-certificate -nc https://zenodo.org/record/4559324/files/tev14_pythia8_ttbar_"$j"_"$i".pkl.bz2
-#   done
-# done
-#
-# bzip2 -d *
+rm -Rf test_tmp_delphes
+mkdir test_tmp_delphes
+cd test_tmp_delphes
+
+mkdir -p experiments
+
+mkdir -p data/pythia8_ttbar
+mkdir -p data/pythia8_ttbar/raw
+mkdir -p data/pythia8_ttbar/processed
+
+mkdir -p data/pythia8_qcd
+mkdir -p data/pythia8_qcd/raw
+mkdir -p data/pythia8_qcd/processed
+
+# now get the ttbar data for training/testing
+cd data/pythia8_ttbar/raw/
+
+for j in {0..9}
+do
+  for i in {0..49}
+  do
+    wget --no-check-certificate -nc https://zenodo.org/record/4559324/files/tev14_pythia8_ttbar_"$j"_"$i".pkl.bz2
+  done
+done
+
+bzip2 -d *
 
 # now get the qcd data for extra validation
-cd data/pythia8_qcd/raw/
+cd ../../pythia8_qcd/raw/
 
 for i in {0..49}
 do
