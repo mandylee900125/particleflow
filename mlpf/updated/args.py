e70cf25bac6c7cefff0f0ac57377094ddd5b9e22
Farouk <farouk.mokhtar@gmail.com>
2021-04-05 14:17:56 -0700

    processing qcd raw data

--- mlpf/updated/args.py
+++ mlpf/updated/args.py
@@ -6,7 +6,7 @@ def parse_args():
 
     # from raw -> processed
     parser.add_argument("--dataset", type=str, default='../../../test_tmp_delphes/data/pythia8_ttbar', help="dataset path", required=True)
-    parser.add_argument("--dataset_qcd", type=str, default='../../../test_tmp_delphes_qcd/data/pythia8_ttbar', help="dataset path", required=True)
+    parser.add_argument("--dataset_qcd", type=str, default='../../../test_tmp_delphes/data/pythia8_qcd', help="dataset path", required=True)
     parser.add_argument("--processed_dir", type=str, help="processed", required=False, default=None)
     parser.add_argument("--num-files-merge", type=int, default=10, help="number of files to merge")
     parser.add_argument("--num-proc", type=int, default=24, help="number of processes")
