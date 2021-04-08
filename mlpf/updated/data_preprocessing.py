63d9e45c006f76796b6681cc899093327a90f261
Farouk <farouk.mokhtar@gmail.com>
2021-04-08 10:53:15 -0700

    d

--- mlpf/updated/data_preprocessing.py
+++ mlpf/updated/data_preprocessing.py
@@ -6,6 +6,7 @@ use_gpu = torch.cuda.device_count()>0
 multi_gpu = torch.cuda.device_count()>1
 multi_gpu=0
 
+#multi_gpu=0
 #define the global base device
 if use_gpu:
     device = torch.device('cuda:0')
