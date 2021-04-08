6e2e7e8812cdd8b4017f75682cd741b924eace82
Farouk <farouk.mokhtar@gmail.com>
2021-04-08 13:54:00 -0700

    d

--- mlpf/updated/training_multigpu.py
+++ mlpf/updated/training_multigpu.py
@@ -162,7 +162,7 @@ def train(model, loader, epoch, optimizer, l1m, l2m, l3m, target_type, device):
             target_p4 = batch.ycand.to(device)
 
         if args.target == "gen":
-            X = batch.to(device)
+            X = batch
             # target_ids = batch.ygen_id
             # target_p4 = batch.ygen
 
