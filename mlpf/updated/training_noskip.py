9a072cf016e23ffb42d1d9b9529d4efc6a181fc1
Farouk <farouk.mokhtar@gmail.com>
2021-05-31 17:09:57 -0700

    f

--- mlpf/updated/training_noskip.py
+++ mlpf/updated/training_noskip.py
@@ -178,7 +178,7 @@ def train(model, loader, epoch, optimizer, alpha, target_type, device):
         conf_matrix += sklearn.metrics.confusion_matrix(target_ids.detach().cpu().numpy(),
                                         np.argmax(cand_ids_one_hot.detach().cpu().numpy(),axis=1), labels=range(6))
 
-        print('{}/{} batch_loss={:.2f} dt={:.1f}s'.format(i, len(loader), loss.item(), t1-t0)), end='\r', flush=True)
+        print('{}/{} batch_loss={:.2f} dt={:.1f}s'.format(i, len(loader), loss.item(), t1-t0), end='\r', flush=True)
 
     losses_1 = np.mean(losses_1)
     losses_2 = np.mean(losses_2)
