99621d93bc884148179d0b1f1a7eb73d544cf8e6
Farouk <farouk.mokhtar@gmail.com>
2021-07-06 16:32:14 -0700

    get_all_data update

--- scripts/get_all_data.sh
+++ scripts/get_all_data.sh
@@ -39,10 +39,10 @@ done
 
 bzip2 -d *
 
-#generate pytorch data files from pkl files
-python3 ../particleflow/mlpf/pytorch/graph_data_delphes.py --dataset data/pythia8_ttbar \
-  --processed_dir data/pythia8_ttbar/processed --num-files-merge 1 --num-proc 1
-
-#generate pytorch data files from pkl files
-python3 ../particleflow/mlpf/pytorch/graph_data_delphes.py --dataset data/pythia8_qcd \
-  --processed_dir data/pythia8_qcd/processed --num-files-merge 1 --num-proc 1
+# #generate pytorch data files from pkl files
+# python3 ../particleflow/mlpf/pytorch/graph_data_delphes.py --dataset data/pythia8_ttbar \
+#   --processed_dir data/pythia8_ttbar/processed --num-files-merge 1 --num-proc 1
+#
+# #generate pytorch data files from pkl files
+# python3 ../particleflow/mlpf/pytorch/graph_data_delphes.py --dataset data/pythia8_qcd \
+#   --processed_dir data/pythia8_qcd/processed --num-files-merge 1 --num-proc 1
