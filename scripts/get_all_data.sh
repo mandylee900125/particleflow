6809727136fc44551adc58363958da1f043b6745
Farouk <farouk.mokhtar@gmail.com>
2021-07-06 17:46:31 -0700

    update get_all_data

--- scripts/get_all_data.sh
+++ scripts/get_all_data.sh
@@ -1,3 +1,5 @@
+# this script assumes you git cloned the repo adn are inside the particleflow/scripts directory
+# you can run the script using ./get_all_data.#!/bin/sh
 
 #!/bin/bash
 set -e
@@ -39,10 +41,12 @@ done
 
 bzip2 -d *
 
-# #generate pytorch data files from pkl files
-# python3 ../particleflow/mlpf/pytorch/graph_data_delphes.py --dataset data/pythia8_ttbar \
-#   --processed_dir data/pythia8_ttbar/processed --num-files-merge 1 --num-proc 1
-#
-# #generate pytorch data files from pkl files
-# python3 ../particleflow/mlpf/pytorch/graph_data_delphes.py --dataset data/pythia8_qcd \
-#   --processed_dir data/pythia8_qcd/processed --num-files-merge 1 --num-proc 1
+cd ../../../
+
+#generate pytorch data files from pkl files
+python3 ../particleflow/mlpf/pytorch/graph_data_delphes.py --dataset data/pythia8_ttbar \
+  --processed_dir data/pythia8_ttbar/processed --num-files-merge 1 --num-proc 1
+
+#generate pytorch data files from pkl files
+python3 ../particleflow/mlpf/pytorch/graph_data_delphes.py --dataset data/pythia8_qcd \
+  --processed_dir data/pythia8_qcd/processed --num-files-merge 1 --num-proc 1
