d9d015c2dffa89bc02da8ad9c4553538f8b4e059
Farouk <farouk.mokhtar@gmail.com>
2021-07-12 11:53:46 -0700

    clf

---
+++ mlpf/updated/LRP/LRP_clf.py
@@ -0,0 +1,318 @@
+import torch
+import torch.nn as nn
+from torch.nn import Sequential as Seq,Linear,ReLU,BatchNorm1d
+from torch_scatter import scatter_mean
+import numpy as np
+import json
+import model_io
+from torch_geometric.utils import to_scipy_sparse_matrix
+import scipy
+import pickle, math, time
+
+from torch_geometric.data import Data
+import networkx as nx
+from torch_geometric.utils.convert import to_networkx
+
+use_gpu = torch.cuda.device_count()>0
+multi_gpu = torch.cuda.device_count()>1
+
+#define the global base device
+if use_gpu:
+    device = torch.device('cuda:0')
+else:
+    device = torch.device('cpu')
+
+class LRP:
+    EPSILON=1e-9
+
+    def __init__(self,model:model_io):
+        self.model=model
+
+    def register_model(model:model_io):
+        self.model=model
+
+    """
+    LRP rules
+    """
+
+    # this rule is wrong.. it is just here because it is much quicker for experimentation and gives the correct dimensions needed for debugging
+    @staticmethod
+    def easy_rule(layer,input,R,index,output_layer,activation_layer):
+        print('l1', layer)
+
+        EPSILON=1e-9
+        a=copy_tensor(input)
+        a.retain_grad()
+        z = layer.forward(a)
+
+        if activation_layer:
+            w = torch.eye(a.shape[1])
+        else:
+            w = layer.weight
+
+        if output_layer: # for the output layer
+            T, W, r = [], [], []
+
+            for i in range(R.shape[1]):
+                T.append(R[:,i].reshape(-1,1))
+                W.append(w[i,:].reshape(1,-1))
+                I = torch.ones_like(R[:,i]).reshape(-1,1)
+
+                Numerator=(a*torch.matmul(T[i],W[i]))
+                Denominator=(a*torch.matmul(I,W[i])).sum(axis=1)
+
+                Denominator = Denominator.reshape(-1,1).expand(Denominator.size()[0],Numerator.size()[1])
+                r.append(torch.abs(Numerator / (Denominator+EPSILON*torch.sign(Denominator))))
+                print('- Finished computing R-scores for output neuron #: ', i)
+                break
+            print(f'- Completed layer: {layer}')
+            return r
+        else:
+            for i in range(len(R)):
+                I = torch.ones_like(R[i])
+
+                Numerator=(a*torch.matmul(R[i],w))
+                Denominator=(a*torch.matmul(I,w)).sum(axis=1)
+
+                Denominator = Denominator.reshape(-1,1).expand(Denominator.size()[0],Numerator.size()[1])
+                R[i]=(torch.abs(Numerator / (Denominator+EPSILON*torch.sign(Denominator))))
+                print('- Finished computing R-scores for output neuron #: ', i)
+                break
+            print(f'- Completed layer: {layer}')
+
+            return R
+
+
+    @staticmethod
+    def eps_rule_before_gravnet(layer, input, R, index, output_layer, activation_layer):
+
+        EPSILON=1e-9
+        a=copy_tensor(input)
+        a.retain_grad()
+        z = layer.forward(a)
+        # basically layer.forward does this: output=(torch.matmul(a,torch.transpose(w,0,1))+b) , assuming the following w & b are retrieved
+
+        if activation_layer:
+            w = torch.eye(a.shape[1])
+        else:
+            w = layer.weight
+            b = layer.bias
+
+        wt = torch.transpose(w,0,1)
+
+        if output_layer:
+            R_list = [None]*R.shape[1]
+            Wt = [None]*R.shape[1]
+            for output_node in range(R.shape[1]):
+                R_list[output_node]=(R[:,output_node].reshape(-1,1).clone())
+                Wt[output_node]=(wt[:,output_node].reshape(-1,1))
+        else:
+            R_list = R
+            Wt = [wt]*len(R_list)
+
+        R_previous=[None]*len(R_list)
+        for output_node in range(len(R_list)):
+            # rep stands for repeated
+            a_rep = a.reshape(a.shape[0],a.shape[1],1).repeat(1,1,R_list[output_node].shape[1])
+            wt_rep = Wt[output_node].reshape(1,Wt[output_node].shape[0],Wt[output_node].shape[1]).repeat(a.shape[0],1,1)
+
+            H = a_rep*wt_rep
+
+            G = H/H.sum(axis=1).reshape(H.sum(axis=1).shape[0],1,H.sum(axis=1).shape[1]).repeat(1,a.shape[1],1).float()
+
+            R_previous[output_node] = (torch.matmul(G, R_list[output_node].reshape(R_list[output_node].shape[0],R_list[output_node].shape[1],1).float()))
+            R_previous[output_node] = R_previous[output_node].reshape(R_previous[output_node].shape[0], R_previous[output_node].shape[1])
+            print('- Finished computing R-scores for output neuron #: ', output_node)
+            break
+
+        print(f'- Completed layer: {layer}')
+        if (torch.allclose(R_previous[output_node].sum(axis=1), R_list[output_node].sum(axis=1))):
+            print('- R score is conserved up to relative tolerance 1e-5')
+        elif (torch.allclose(R_previous[output_node].sum(axis=1), R_list[output_node].sum(axis=1), rtol=1e-4)):
+            print('- R score is conserved up to relative tolerance 1e-4')
+        elif (torch.allclose(R_previous[output_node].sum(axis=1), R_list[output_node].sum(axis=1), rtol=1e-3)):
+            print('- R score is conserved up to relative tolerance 1e-3')
+        elif (torch.allclose(R_previous[output_node].sum(axis=1), R_list[output_node].sum(axis=1), rtol=1e-2)):
+            print('- R score is conserved up to relative tolerance 1e-2')
+        elif (torch.allclose(R_previous[output_node].sum(axis=1), R_list[output_node].sum(axis=1), rtol=1e-1)):
+            print('- R score is conserved up to relative tolerance 1e-1')
+
+        return R_previous
+
+    @staticmethod
+    def eps_rule_after_gravnet(layer, input, R, index, activation_layer):
+
+        EPSILON=1e-9
+        a=copy_tensor(input)
+        a.retain_grad()
+        # basically layer.forward does this: output=(torch.matmul(a,torch.transpose(w,0,1))+b) , assuming the following w & b are retrieved
+
+        if activation_layer:
+            w = torch.eye(a.shape[1])
+        else:
+            w = layer.weight
+            b = layer.bias
+
+        wt = torch.transpose(w,0,1)
+
+        R_list = R
+        Wt = [wt]*len(R_list)
+
+        R_previous=[None]*len(R_list)
+        for output_node in range(len(R_list)):
+        # rep stands for repeated
+            a_rep = a.reshape(1,a.shape[0],a.shape[1],1).repeat(R[output_node].shape[0],1,1,R[output_node].shape[2])
+            wt_rep = wt.reshape(1,1,wt.shape[0],wt.shape[1]).repeat(R[output_node].shape[0],a.shape[0],1,1)
+
+            H = a_rep*wt_rep
+
+            G = H/H.sum(axis=1).reshape(H.sum(axis=1).shape[0],1,H.sum(axis=1).shape[1]).repeat(1,a.shape[1],1).float()
+
+            R_previous[output_node] = torch.matmul(G, R[output_node].reshape(R[output_node].shape[0],R[output_node].shape[1],1).float())
+
+            R_previous[output_node] = R_previous[output_node].reshape(R_previous[output_node].shape[0], R_previous[output_node].shape[1])
+            print('- Finished computing R-scores for output neuron #: ', output_node)
+            break
+
+        print(f'- Completed layer: {layer}')
+        if (torch.allclose(R_previous[output_node].sum(axis=1), R_list[output_node].sum(axis=1))):
+            print('- R score is conserved up to relative tolerance 1e-5')
+        elif (torch.allclose(R_previous[output_node].sum(axis=1), R_list[output_node].sum(axis=1), rtol=1e-4)):
+            print('- R score is conserved up to relative tolerance 1e-4')
+        elif (torch.allclose(R_previous[output_node].sum(axis=1), R_list[output_node].sum(axis=1), rtol=1e-3)):
+            print('- R score is conserved up to relative tolerance 1e-3')
+        elif (torch.allclose(R_previous[output_node].sum(axis=1), R_list[output_node].sum(axis=1), rtol=1e-2)):
+            print('- R score is conserved up to relative tolerance 1e-2')
+        elif (torch.allclose(R_previous[output_node].sum(axis=1), R_list[output_node].sum(axis=1), rtol=1e-1)):
+            print('- R score is conserved up to relative tolerance 1e-1')
+
+        return R_previous
+
+    @staticmethod
+    def message_passing_rule(layer, input, R, big_list, edge_index, edge_weight, after_message, before_message, index):
+
+        b=Data()
+        b['edge_index']=edge_index
+        b['num_nodes']=edge_index[0].shape[0]
+        b['edge_weight']=edge_weight
+
+        G = to_networkx(b, edge_attrs=['edge_weight'], to_undirected=True, remove_self_loops=False)
+
+        def nodes_connected(u, v):
+            return u in G.neighbors(v)
+
+        R_tensor_per_all_nodes=[None]*len(R)
+        for output_node in range(len(R)):
+            R_tensor_per_all_nodes[output_node]=torch.zeros(R[output_node].shape[0],R[output_node].shape[0],R[output_node].shape[1])
+
+            for node_i in range(R[output_node].shape[0]):
+                R_tensor_per_all_nodes[output_node][node_i][node_i]=R[output_node][node_i]
+                
+            print('- Finished computing R-scores for for all nodes for output neuron # : ', output_node)
+            break
+
+        print('- Completed layer: Message Passing')
+
+        return R_tensor_per_all_nodes
+
+
+    """
+    explanation functions
+    """
+
+    def explain(self,
+                to_explain:dict,
+                save:bool=True,
+                save_to:str="./relevance.pt",
+                sort_nodes_by:int=0,
+                signal=torch.tensor([1,0,0,0,0,0],dtype=torch.float32).to(device),
+                return_result:bool=False):
+
+        inputs=to_explain["inputs"]
+        pred=to_explain["pred"]
+        edge_index=to_explain["edge_index"]
+        edge_weight=to_explain["edge_weight"]
+        after_message=to_explain["after_message"]
+        before_message=to_explain["before_message"]
+
+        start_index=self.model.n_layers                  ##########################
+        print('Total number of layers (including activation layers):', start_index)
+        to_explain['R']["layer"+str(start_index)]=copy_tensor(pred)
+
+        ### loop over each single layer
+        R=to_explain["R"]["layer"+str(start_index)]
+        big_list=[]
+        BIG_LIST={}
+        for index in range(start_index+1, 1,-1):
+            print(f"Explaining layer {1+start_index+1-index}/{start_index+1-1}")
+            R, big_list  = self.explain_single_layer(R, to_explain, edge_index, edge_weight, after_message, before_message, big_list, start_index+1, index)
+            if len(big_list)!=0:
+                BIG_LIST["layer"+str(index-2)]=big_list
+
+        return to_explain['R'], BIG_LIST
+
+    def explain_single_layer(self, R, to_explain, edge_index, edge_weight, after_message, before_message, big_list, output_layer_index, index=None, name=None):
+        # preparing variables required for computing LRP
+        layer=self.model.get_layer(index=index,name=name)
+
+        if name is None:
+            name=self.model.index2name(index)
+        if index is None:
+            index=self.model.name2index(name)
+
+        input=to_explain['A'][name]
+
+        if index==output_layer_index:
+            output_layer_bool=True
+        else:
+            output_layer_bool=False
+
+        # it works out of the box that the conv1.lin_s layer which we don't care about is in the same place of the message passing.. so we can just replace its action
+        if 'conv1.lin_s' in str(name):
+            big_list = self.message_passing_rule(layer, input, R, big_list, edge_index, edge_weight, after_message, before_message, index)
+            return R, big_list
+
+        # if you haven't hit the message passing step yet
+        if len(big_list)==0:
+            if 'Linear' in str(layer):
+                R = self.easy_rule(layer, input, R, index, output_layer_bool, activation_layer=False)
+                to_explain["R"]["layer"+str(index-2)]=R
+            elif 'LeakyReLU' or 'ELU' in str(layer):
+                R = self.easy_rule(layer, input, R, index, output_layer_bool, activation_layer=True)
+                to_explain["R"]["layer"+str(index-2)]=R
+        else:
+            # for i in range(len(big_list)):
+            #     if 'Linear' in str(layer):
+            #         big_list[i] = self.easy_rule(layer, input, big_list[i], index, output_layer_bool, activation_layer=False)
+            #     elif 'LeakyReLU' or 'ELU' in str(layer):
+            #         big_list[i] = self.easy_rule(layer, input, big_list[i], index, output_layer_bool, activation_layer=True)
+            #     break
+
+            if 'Linear' in str(layer):
+                big_list = self.eps_rule_after_gravnet(layer, input, big_list, index, activation_layer=False)
+            elif 'LeakyReLU' or 'ELU' in str(layer):
+                big_list =  self.eps_rule_after_gravnet(layer, input, big_list, index, activation_layer=True)
+
+        return R, big_list
+
+def copy_tensor(tensor,dtype=torch.float32):
+    """
+    create a deep copy of the provided tensor,
+    outputs the copy with specified dtype
+    """
+
+    return tensor.clone().detach().requires_grad_(True).to(device)
+
+
+##-----------------------------------------------------------------------------
+#
+# arep=torch.transpose(a[0].repeat(6, 1),0,1)   # repeat it 6 times
+# H=arep*wt
+#
+# G = H/H.sum(axis=0).float()
+#
+# Num = torch.matmul(G, R[0].float())
+#
+# print('Num.sum()', Num.sum())
+#
+# print(R[0].sum())
---
+++ mlpf/updated/LRP/main_clf.py
@@ -0,0 +1,232 @@
+from glob import glob
+import sys, os
+import os.path as osp
+import pickle as pkl
+import math, time, numba, tqdm
+import numpy as np
+import pandas as pd
+import sklearn
+from sklearn.metrics import accuracy_score, confusion_matrix
+import matplotlib, mplhep
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
+
+#Check if the GPU configuration has been provided
+import torch
+use_gpu = torch.cuda.device_count()>0
+multi_gpu = torch.cuda.device_count()>1
+
+try:
+    if not ("CUDA_VISIBLE_DEVICES" in os.environ):
+        import setGPU
+        if multi_gpu:
+            print('Will use multi_gpu..')
+            print("Let's use", torch.cuda.device_count(), "GPUs!")
+        else:
+            print('Will use single_gpu..')
+except Exception as e:
+    print("Could not import setGPU, running CPU-only")
+
+#define the global base device
+if use_gpu:
+    device = torch.device('cuda:0')
+else:
+    device = torch.device('cpu')
+
+import torch_geometric
+import torch.nn as nn
+import torch.nn.functional as F
+import torch_geometric.transforms as T
+from torch.nn import Sequential as Seq, Linear as Lin, ReLU
+from torch_scatter import scatter_mean
+from torch_geometric.nn.inits import reset
+from torch_geometric.data import Data, DataLoader, DataListLoader, Batch
+from torch_geometric.nn import GravNetConv
+from torch.utils.data import random_split
+import torch_cluster
+
+sys.path.insert(1, '../')
+sys.path.insert(1, '../../../plotting/')
+sys.path.insert(1, '../../../mlpf/plotting/')
+import args
+from args import parse_args
+from graph_data_delphes import PFGraphDataset, one_hot_embedding
+from data_preprocessing import data_to_loader_ttbar, data_to_loader_qcd
+from model_LRP_clf import PFNet7
+
+from LRP_clf import LRP
+from model_io import model_io
+
+import networkx as nx
+from torch_geometric.utils.convert import to_networkx
+from tabulate import tabulate
+
+# NOTE: this script works by loading an already trained model
+
+#Ignore divide by 0 errors
+np.seterr(divide='ignore', invalid='ignore')
+
+#Get a unique directory name for the model
+def get_model_fname(dataset, model, n_train, n_epochs, lr, target_type, batch_size, task, title):
+    model_name = type(model).__name__
+    model_params = sum(p.numel() for p in model.parameters())
+    import hashlib
+    model_cfghash = hashlib.blake2b(repr(model).encode()).hexdigest()[:10]
+    model_user = os.environ['USER']
+
+    model_fname = '{}_{}_ntrain_{}_nepochs_{}_batch_size_{}_lr_{}_{}'.format(
+        model_name,
+        target_type,
+        n_train,
+        n_epochs,
+        batch_size,
+        lr,
+        task,
+        title)
+    return model_fname
+
+def map_classid_to_classname(id):
+    if id==0:
+        return 'null'
+    if id==1:
+        return 'charged hadron'
+    if id==2:
+        return 'neutral hadron'
+    if id==3:
+        return 'photon'
+    if id==4:
+        return 'electron'
+    if id==5:
+        return 'muon'
+
+if __name__ == "__main__":
+
+    # args = parse_args()
+
+    # the next part initializes some args values (to run the script not from terminal)
+    class objectview(object):
+        def __init__(self, d):
+            self.__dict__ = d
+
+    args = objectview({'train': False, 'n_train': 1, 'n_valid': 1, 'n_test': 2, 'n_epochs': 2, 'patience': 100, 'hidden_dim':256, 'input_encoding': 12, 'encoding_dim': 64,
+    'batch_size': 1, 'model': 'PFNet7', 'target': 'gen', 'dataset': '../../../test_tmp_delphes/data/pythia8_ttbar', 'dataset_qcd': '../../../test_tmp_delphes/data/pythia8_qcd',
+    'outpath': '../../../prp/models/LRP/', 'optimizer': 'adam', 'lr': 0.001, 'alpha': 1, 'dropout': 0,
+    'space_dim': 4, 'propagate_dimensions': 22,'nearest': 16, 'overwrite': True,
+    'load': True, 'load_epoch': 14, 'load_model': 'LRP_clf_PFNet7_gen_ntrain_1_nepochs_15_batch_size_1_lr_0.001_alpha_0.0002_clf_noskip_nn1',
+    'classification_only': True, 'nn1': True, 'conv2': False, 'nn3': False, 'title': '', 'explain': True})
+
+    # define the dataset (assumes the data exists as .pt files in "processed")
+    print('Processing the data..')
+    full_dataset_ttbar = PFGraphDataset(args.dataset)
+    full_dataset_qcd = PFGraphDataset(args.dataset_qcd)
+
+    # constructs a loader from the data to iterate over batches
+    print('Constructing data loaders..')
+    train_loader, valid_loader = data_to_loader_ttbar(full_dataset_ttbar, args.n_train, args.n_valid, batch_size=args.batch_size)
+    test_loader = data_to_loader_qcd(full_dataset_qcd, args.n_test, batch_size=args.batch_size)
+
+    # element parameters
+    input_dim = 12
+
+    #one-hot particle ID and momentum
+    output_dim_id = 6
+    output_dim_p4 = 6
+
+    patience = args.patience
+
+    model_classes = {"PFNet7": PFNet7}
+
+    model_class = model_classes[args.model]
+    model_kwargs = {'input_dim': input_dim,
+                    'hidden_dim': args.hidden_dim,
+                    'input_encoding': args.input_encoding,
+                    'encoding_dim': args.encoding_dim,
+                    'output_dim_id': output_dim_id,
+                    'output_dim_p4': output_dim_p4,
+                    'space_dim': args.space_dim,
+                    'propagate_dimensions': args.propagate_dimensions,
+                    'nearest': args.nearest,
+                    'target': args.target,
+                    'nn1': args.nn1,
+                    'nn3': args.nn3}
+
+    print('Loading a previously trained model..')
+    model = model_class(**model_kwargs)
+    outpath = args.outpath + args.load_model
+    PATH = outpath + '/epoch_' + str(args.load_epoch) + '_weights.pth'
+
+    state_dict = torch.load(PATH, map_location=device)
+
+    # if model was trained using DataParallel then we have to load it differently
+    if "DataParallel" in args.load_model:
+        state_dict = torch.load(PATH, map_location=device)
+        from collections import OrderedDict
+        new_state_dict = OrderedDict()
+        for k, v in state_dict.items():
+            name = k[7:] # remove module.
+            new_state_dict[name] = v
+            # print('name is:', name)
+        state_dict=new_state_dict
+
+    model.load_state_dict(state_dict)
+
+    if args.explain:
+        model.eval()
+        print(model)
+
+        signal =torch.tensor([1,0,0,0,0,0],dtype=torch.float32).to(device)
+
+        # create some hooks to retrieve intermediate activations
+        activation = {}
+        hooks={}
+
+        def get_activation(name):
+            def hook(model, input, output):
+                activation[name] = input[0]
+            return hook
+
+        for name, module in model.named_modules():
+            if (type(module)==nn.Linear) or (type(module)==nn.LeakyReLU) or (type(module)==nn.ELU):
+                hooks[name] = module.register_forward_hook(get_activation("." + name))
+            # print(name)
+
+        for i, batch in enumerate(train_loader):
+            t0 = time.time()
+
+            if multi_gpu:
+                X = batch
+            else:
+                X = batch.to(device)
+
+            if i==0:
+                # code can be written better but i am a physicist and i have no time for this
+                # basically i run at least one forward pass to get the activations to use their shape in defining the LRP layers
+                pred_ids_one_hot, pred_p4, gen_ids_one_hot, gen_p4, cand_ids_one_hot, cand_p4, edge_index, edge_weight, after_message, before_message = model(X)
+                model=model_io(model,state_dict,dict(),activation)
+                explainer=LRP(model)
+
+            else:
+                pred_ids_one_hot, pred_p4, gen_ids_one_hot, gen_p4, cand_ids_one_hot, cand_p4, edge_index, edge_weight, after_message, before_message = model.model(X)
+
+            to_explain={"A":activation,"inputs":dict(x=X.x,
+                                                batch=X.batch),"y":gen_ids_one_hot,"R":dict(),"pred":pred_ids_one_hot,
+                                                "edge_index":edge_index, "edge_weight":edge_weight, "after_message":after_message, "before_message":before_message}
+
+            model.set_dest(to_explain["A"])
+
+            # both R_before_gravnet & R_after_gravnet are dictionaries with keys 'layer15', 'layer14', ...
+            # for each key:
+            # R_before_gravnet: is one big tensor for all nodes.. the rows of the map represent nodes.. since nodes haven't talked to each other yet, the rows haven't mixed up yet
+            # R_after_gravnet: is one big tensor for each node.. the rows have mixed up differently for each node, depending on its neighbours
+            R_before_gravnet, R_after_gravnet = explainer.explain(to_explain,save=False,return_result=True, signal=signal)
+
+            with open(args.outpath+'/'+args.load_model+'/R_before_gravnet.pkl', 'wb') as f:
+                pkl.dump(R_before_gravnet, f)
+            with open(args.outpath+'/'+args.load_model+'/R_after_gravnet.pkl', 'wb') as f:
+                pkl.dump(R_after_gravnet, f)
+            break
+## -----------------------------------------------------------
+# # to retrieve a stored variable in pkl file
+# import pickle
+# with open('../../test_tmp_delphes/experiments/PFNet7_gen_ntrain_2_nepochs_3_batch_size_3_lr_0.0001/confusion_matrix_plots/cmT_normed_epoch_0.pkl', 'rb') as f:  # Python 3: open(..., 'rb')
+#     a = pickle.load(f)
